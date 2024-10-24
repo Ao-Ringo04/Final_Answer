80389ee6aaac32e37236c5733cf89947e94f519c
Aoi.M'04 <50298097+Ao-Ringo04@users.noreply.github.com>
2024-10-24 23:50:33 +0900

    change1-2

--- python/ex1_web-scraping/1-2.py
+++ python/ex1_web-scraping/1-2.py
@@ -7,6 +7,6 @@ from selenium.webdriver.common.by import By
 driver = webdriver.Chrome()
 driver.get("https://r.gnavi.co.jp/area/jp/rs/?fwp=東京&date=2024101")
 url_cls = driver.find_elements(By.CLASS_NAME, "style_titleLink__oiHVJ")
-あいうえお
+
 
 
